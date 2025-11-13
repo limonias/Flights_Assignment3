e4d388654d760e040d39b50782f41a8c179ed3af
Taburetochka <stiv.kimmel05@gmail.com>
2025-11-13 18:16:21 +0200

    fix: make all types string

--- raw_data.sql
+++ raw_data.sql
@@ -1,12 +1,12 @@
 
 CREATE OR REPLACE TABLE flight_to_2026.raw_flight_schedule (
-    Flight_ID INTEGER,
+    Flight_ID STRING,
     Airline_Code STRING,
     Origin_Airport_Code STRING,
     Destination_Airport_Code STRING,
     Scheduled_Departure_Time TIMESTAMP,
     Actual_Departure_Time TIMESTAMP, 
-    Gate_ID INTEGER,
+    Gate_ID STRING,
     Status STRING,
     Load_TimeStamp TIMESTAMP,
 ); 
@@ -14,13 +14,13 @@ CREATE OR REPLACE TABLE flight_to_2026.raw_passengers (
     Booking_ID STRING,
     Flight_ID STRING,
     Passenger_ID STRING,
-    Ticket_Price FLOAT, 
-    Luggage_Weight_KG FLOAT
+    Ticket_Price STRING, 
+    Luggage_Weight_KG STRING
 ); 
 
 CREATE OR REPLACE TABLE flight_to_2026.raw_airline (
     Airline_Code STRING,
     Airline_Name STRING,
     Country STRING,
-    Is_Low_Cost_Carrier_Str BOOLEAN 
+    Is_Low_Cost_Carrier_Str STRING 
 );
