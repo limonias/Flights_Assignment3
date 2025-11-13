641e7799f51521b4eed9fd33786d9760b1bf87f8
Kuzmin Yaroslav <yarickkuzmin@gmail.com>
2025-11-13 12:49:22 +0200

    Update stage_layer.sql

--- stage_layer.sql
+++ stage_layer.sql
@@ -1,11 +1,13 @@
+
 CREATE OR REPLACE TABLE `flight_to_2026.stg_airlines` AS (
   SELECT 
-    UPPER(TRIM(Airline_Code)) AS airline_code, TRIM(Airline_Name) AS airline_name,
-    TRIM(Country) AS country, SAFE_CAST(Is_Low_Cost_Carrier_Str AS BOOL) AS is_low_cost_carrier,
+    UPPER(TRIM(CAST(Airline_Code AS STRING))) AS airline_code, 
+    TRIM(Airline_Name) AS airline_name, TRIM(Country) AS country, 
+    SAFE_CAST(Is_Low_Cost_Carrier_Str AS BOOL) AS is_low_cost_carrier,
     CURRENT_TIMESTAMP() AS load_timestamp
   FROM `flight_to_2026.raw_airline`
   WHERE Airline_Code IS NOT NULL
-  QUALIFY ROW_NUMBER() OVER(PARTITION BY UPPER(TRIM(Airline_Code)) ORDER BY Airline_Code) = 1
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY UPPER(TRIM(CAST(Airline_Code AS STRING))) ORDER BY Airline_Code) = 1
 );
 
 
@@ -17,8 +19,9 @@ CREATE OR REPLACE TABLE flight_to_2026.stg_flight_schedule AS (
     UPPER(TRIM(CAST(Destination_Airport_Code AS STRING))) AS destination_airport_code,
     SAFE.PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S', Scheduled_Departure_Time) AS scheduled_departure_time,
     SAFE.PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S', Actual_Departure_Time) AS actual_departure_time,
-    NULLIF(TRIM(CAST(Gate_ID AS STRING)), '') AS gate_id, 
-    TRIM(Status) AS status, CURRENT_TIMESTAMP() AS load_timestamp
+    FARM_FINGERPRINT(NULLIF(TRIM(CAST(Gate_ID AS STRING)), '')) AS gate_id, 
+    TRIM(Status) AS status, 
+    CURRENT_TIMESTAMP() AS load_timestamp
   FROM `flight_to_2026.raw_flight_schedule`
   WHERE Flight_ID IS NOT NULL
   QUALIFY ROW_NUMBER() OVER(PARTITION BY UPPER(TRIM(CAST(Flight_ID AS STRING))) ORDER BY Scheduled_Departure_Time) = 1
